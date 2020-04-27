422be6babab28fe2885d5c2a57fa219a59fddf20
deveshlashkari <deveshlashkari@gmail.com>
2020-04-27 12:28:27 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Hello</p>
         <a
           className="App-link"
           href="https://reactjs.org"
